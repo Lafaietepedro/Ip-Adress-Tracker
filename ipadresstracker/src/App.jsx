46407e79424ed434da552add370a33303a20df19
Lafaietepedro <lafaietepedro3@gmail.com>
2024-08-26 20:16:37 -0300

    interface

--- ipadresstracker/src/App.jsx
+++ ipadresstracker/src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ipData, setIpData] = useState(null);
+  const [inputValue, setInputValue] = useState('')
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    // Inicializar o mapa quando o componente for montado
+    const map = L.map('map').setView([40.7128, -74.0060], 13);
+
+    // Adicionando o tile layer do mapa
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Adicionando um marcador no mapa
+    L.marker([40.7128, -74.0060]).addTo(map)
+      .bindPopup('Brooklyn, NY 10001')
+      .openPopup();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div className='banner'>
+        <h1>Ip Adress Tracker</h1>
+        <input type="text" placeholder='Search for any IP address or domain'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="show_data">
+        <div className="data_box1">
+          <h2>IP ADRESS</h2>
+          <p>pass</p>
+        </div>
+        <div className="data_box2">
+          <h2>LOCATION</h2>
+          <p>pass</p>
+        </div>
+        <div className="data_box3">
+          <h2>TIMEZONE</h2>
+          <p>pass</p>
+        </div>
+        <div className="data_box4">
+          <h2>ISP</h2>
+          <p>pass</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="map_container">
+
+      </div>
+    </div>
   )
 }
 
